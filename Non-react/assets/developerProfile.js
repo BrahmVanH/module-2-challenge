02861a1ef57e0b72f486853d5e2ca828eacda387
Brahm V <brahmvanh@gmail.com>
2023-04-27 12:30:00 -0400

    Imported React-based portfolio version to deploy on brahmvanhouzen.studio

---
+++ Non-react/assets/developerProfile.js
@@ -0,0 +1,15 @@
+// Function: on page load => display gallery of projects
+// Function: remove 'showcase' content
+// Function: show 'bio'
+// Function: show 'contact'
+// Function: on click 'bio' => showBio
+// Function: on click 'contact' => showContact
+// Function: on click 'Brahm Van Houzen' => direct user to main webpage 
+
+
+
+
+
+
+
+
