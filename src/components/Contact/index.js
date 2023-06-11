6ba844a131886a067ebee2f43e53d5daa7cc2a18
Brahm V <brahmvanh@gmail.com>
2023-06-11 02:25:10 -0400

    Added animation to everything else

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import gsap, { Power1 } from 'gsap';
+
 import ContactCard from './contactInformation';
 import ContactForm from './contactForm'
 
 export default function Contact() {
+	useEffect(() => {
+		gsap.from(
+			'.contact-container',
+
+			{
+				opacity: 0,
+				y: 60,
+				duration: 1,
+				ease: Power1.easeIn,
+			}
+		);
+	}, []);
 	return (
-		<div className='d-flex justify-content-center row align-items-center'>
+		<div className='contact-container d-flex justify-content-center row align-items-center'>
 			<ContactForm />
 			<ContactCard />
 		</div>
